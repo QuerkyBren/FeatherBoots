13f06088d104711ed6f75769dcad8ca9eb808ecd
QuerkyBren <robloxbren@gmail.com>
2017-09-03 19:05:42 +1000

    fixed it

--- src/main.c
+++ src/main.c
@@ -3,38 +3,25 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct stringArray
+int string_to_array(char *filecontents, char target[10][20])
 {
-    char *string;
+    int n=0,i,j=0;
 
-}stringArray;
-
-const char delim[2] = " ";
-
-int string_to_array(char *filecontents)
-{
-    char *token;
-    token = strtok(filecontents, delim);
-
-    int i;
-    int dirtyContentsLength;
-    stringArray newContents[100];
-
-    for(i = 0; i < 100; i++)
+    for(i=0;TRUE;i++)
     {
-        newContents[i].string = "";
+        if(filecontents[i]!=' '){
+            target[n][j++]=filecontents[i];
+        }
+        else{
+            target[n][j++]="\0";
+            n++;
+            j=0;
+        }
+        if(filecontents[i]=='\0'){
+            break;
+        }
     }
-
-    i = 0;
-
-    while (token != NULL)
-    {
-        newContents[i].string = token;
-        i++;
-        token = strtok(NULL, delim);
-    }
-
-    return newContents;
+    return n;
 }
 
 int open_file(char filename[30])
@@ -62,23 +49,30 @@ int lex(char filecontents[30])
 
 int main(int argc, char *argv[] )
 {
-        const char *cleanContents;
+       int n;
+        int i;
         char *messyContents;
         char input[30];
+
         printf("What is the filename? ");
-        scanf("%s", input);
+        scanf("%s\n", input);
+        printf("Reading Contents...");
         messyContents = open_file(input);
-        cleanContents = string_to_array(messyContents);
+        printf("The original contents are: %s", messyContents);
+        printf("Contents Read!");
+        printf("Running Lexer...");
+        char cleanContents[10][20];
+        printf("    Splitting up File Contents...");
+        n = string_to_array(messyContents,cleanContents);
+        printf("    File Contents Split!")
+        printf("    Comparing Words to Dictionary...")
+        lex()
 
-        int contentsLength = sizeof(cleanContents) / sizeof(cleanContents[0]);
-        int i;
-        for(i = 0; i < contentsLength; i++)
-        {
-            printf("%s\n", cleanContents[i]);
-        }
+        /*printf("The new contents are: \n");
+        for(i=0;i<=n;i++){
+            printf("%s ",cleanContents[i]);
+        }*/
 
-        printf("Done");
         return 0;
-
 
 }
